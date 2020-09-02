80ea59760ff5bf313d03ce1cf24874b0acbda5d0
snilloO <vega0.0@outlook.com>
2020-09-02 12:08:05 -0400

    update anchors

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ class Config:
         self.sizes_w = [1]*len(self.sizes)
         self.sizes_w[-1]+=3  
         self.nms_threshold = 0.5
-        self.dc_threshold = 0.1
+        self.dc_threshold = 0.4
         
         #loss args
         #self.anchors = [[0.26533935,0.33382434],[0.66550966,0.56042827],[0.0880948,0.11774004]] #w,h normalized by max size
         #self.anchors = [[0.76822971,0.57259308],[0.39598597,0.47268035],[0.20632625,0.26720238],[0.07779112,0.10330848]] 
-        self.anchors=[[34.3, 41.965], [66.143, 110.928], [149.002, 121.749], [152.413, 174.758], [330.323, 261.792], [364.985, 331.075]]
-        self.anchor_divide=[(4,5),(2,3),(0,1)]
+        self.anchors=[[28.112, 38.894], [51.368, 59.001], [83.605, 117.711], [122.2, 125.505], [123.953, 139.515], [219.663, 227.468], [280.433, 237.059], [391.719, 298.828], [395.413, 361.471]]
+        self.anchor_divide=[(6,7,8),(3,4,5),(0,1,2)]
         self.anchor_num = len(self.anchors)
         self.obj_scale = 2.5
         self.noobj_scale = 0.5
@@ -34,13 +34,13 @@ class Config:
             self.crop = 0.2
             self.valid_scale = 0.25
             #train_setting
-            self.lr = 0.1
+            self.lr = 1e-3
             self.weight_decay=5e-4
             self.momentum = 0.9
             #lr_scheduler
-            self.min_lr = 1e-3
-            self.lr_factor = 0.1
-            self.patience = 1
+            self.min_lr = 1e-8
+            self.lr_factor = 0.25
+            self.patience = 10
             #exp_setting
             self.save_every_k_epoch = 10
             self.val_every_k_epoch = 10
