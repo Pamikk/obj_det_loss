b95f4f8978100286649ff997313c617f26ca9233
snilloO <vega0.0@outlook.com>
2021-02-21 17:27:16 -0500

    clear output and fix a bug

--- utils.py
+++ utils.py
@@ -268,20 +268,17 @@ def cal_tp_per_item_wo_cls(pds,gts,threshold=0.5):
     nc = pdbboxes.shape[0]
     mc = gtbboxes.shape[0]
     selected = np.zeros(mc)
-    sel_ious = np.zeros(mc)
     for i in range(nc):
         if mc == 0:
             break
         pdbbox = pdbboxes[i]
         ious = iou_wt_center_np(pdbbox,gtbboxes)
-        mask = (ious>=threshold) & (selected!=1)
-        ious = ious[mask]
-        if len(ious)>0:
-            best = ious.argmax()
+        best = ious.argmax()
+        iou = ious.max()
+        if (selected[best]!=1) and (iou > threshold):
             selected[best] = 1
             tps[pd_idx[i]] = 1.0
-            mc -= 1
-            sel_ious[best] = ious.max()         
+            mc -= 1      
     return [tps,scores,pds[:,-1]]
 def xyhw2xy(boxes_):
     boxes = boxes_.clone()
